785802920372b7dd92fd449921dc2977b6b3e439
Omid Kashan <omid@antilong.com>
2025-08-30 16:18:18 +0100

    chore: configure astro output

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,15 @@
-// @ts-check
 import { defineConfig } from "astro/config"
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://astrodex.29b6.io",
+  output: "static",
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: "_static/[name]-[hash][extname]",
+        },
+      },
+    },
+  },
 })
