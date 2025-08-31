8ccfc68643254281f61c123a8000d79c176294fd
Omid Kashan <omid@antilong.com>
2025-08-31 19:31:15 +0100

    chore: add image path to consts

--- constants.ts
+++ constants.ts
@@ -1,2 +1,4 @@
 export const CHARS = "abcdefghijklmnopqrstuvwxyz".split("")
 export const GENS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+export const IMAGE_PATH =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
