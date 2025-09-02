7a2771b1dbffd28ab08d4b43034b96d7e01af0d7
Omid Kashan <omid@antilong.com>
2025-09-03 00:55:34 +0100

    fix: extract table logic again

---
+++ src/components/scripts/table.ts
@@ -0,0 +1,168 @@
+import type { Pokemon, Species } from "../../types"
+const html = String.raw
+
+const dialog = document.querySelector("dialog.lightbox")! satisfies HTMLDialogElement
+const table = document.querySelector("table.list")! satisfies HTMLElement
+const header = dialog.querySelector("header")! satisfies HTMLElement
+const picture = dialog.querySelector("picture")! satisfies HTMLElement
+
+const state: {
+  data: Species[]
+  id: number
+  formid: number
+} = {
+  data: (await getData()) ?? [],
+  id: 0,
+  formid: 0,
+}
+
+attach()
+
+function attach() {
+  table.addEventListener("click", async (event) => {
+    if (event.target instanceof HTMLElement) {
+      if (event.target.nodeName === "TD") {
+        const { id, formid, name } = event.target.parentElement!.dataset
+        if (id && formid && name) {
+          state.id = +id
+          state.formid = +formid
+          const src = event.target.parentNode!.querySelector("img")!.src
+          await updateDialog(state.id, state.formid, name, src)
+          dialog.showModal()
+        }
+      } else if (event.target.nodeName === "TH") {
+        const { sort, type } = event.target.dataset
+        if (sort) {
+          sortRows(sort, type, event.target)
+        }
+      }
+    }
+  })
+
+  dialog.addEventListener("keydown", async (event) => {
+    const action = event.key === "ArrowRight" ? "next" : event.key === "ArrowLeft" ? "prev" : false
+
+    if (action) {
+      const button = dialog.querySelector(`button[data-action=${action}]`)! satisfies HTMLElement
+      button.style.animation = "press 0.2s ease-out forwards"
+      button.focus()
+      await getSibling(action)
+    }
+  })
+
+  dialog.addEventListener("keyup", (event) => {
+    const action = event.key === "ArrowRight" ? "next" : event.key === "ArrowLeft" ? "prev" : false
+    if (action) {
+      const button = dialog.querySelector(`button[data-action=${action}]`)! satisfies HTMLElement
+      button.style.animation = "press 0.2s ease-out reverse"
+    }
+  })
+
+  dialog.addEventListener("click", async (event) => {
+    if (event.target instanceof HTMLElement) {
+      if (event.target.nodeName === "BUTTON") {
+        const { action } = event.target.dataset
+        console.log(action)
+        if (action === "close") {
+          dialog.close()
+        } else {
+          action && (await getSibling(action))
+        }
+      }
+    }
+  })
+}
+
+async function getKeys(): Promise<{ remoteKey: string; localKey: string; isStale: boolean }> {
+  const remoteKey = await fetch("/meta.json")
+    .then((b) => b.json())
+    .then((d) => d.key)
+
+  const localKey = String(localStorage.getItem("astrodex.key"))
+
+  return { remoteKey, localKey, isStale: remoteKey !== localKey }
+}
+
+async function getData(): Promise<Species[] | null> {
+  const { remoteKey, isStale } = await getKeys()
+
+  if (isStale || !localStorage.hasOwnProperty("astrodex.data")) {
+    const data = await fetch("/data/data.json", { cache: "no-cache" }).then(async (b) => {
+      const d = await b.text()
+      localStorage.setItem("astrodex.data", d)
+      localStorage.setItem("astrodex.key", remoteKey)
+      return d
+    })
+    return JSON.parse(data)
+  } else {
+    return JSON.parse(localStorage.getItem("astrodex.data") ?? "")
+  }
+}
+
+async function getSibling(action: string): Promise<void> {
+  const node = document.querySelector(`tr[data-formid="${state.formid}"]`)!
+  let target = action === "prev" ? node.previousElementSibling : node.nextElementSibling
+
+  if (target instanceof HTMLElement && target.nodeName === "TR") {
+    const { id, formid, name } = target.dataset
+    const src = target.querySelector("img")!.src
+
+    if (id && name && formid) {
+      state.formid = +formid
+      state.id = +id
+      await updateDialog(state.id, state.formid, name, src)
+    }
+  }
+}
+
+function getDetails(
+  id: number,
+  formid: number
+): { species: Species | undefined; form: Pokemon | undefined; defaultForm: Pokemon | undefined } {
+  const species = state.data.find((p) => p.id === id)
+  const form = species && species.family.flatMap((f) => f.forms).find((form) => form.id === formid)
+  const defaultForm = species?.family.find((f) => f.isDefault)?.forms.find((f) => f.isDefault)
+
+  return { species, form, defaultForm }
+}
+
+async function updateDialog(id: number, formid: number, name: string, src: string): Promise<void> {
+  const { species, form, defaultForm } = getDetails(id, formid)
+
+  if (species && form && defaultForm) {
+    dialog.dataset.t1 = form.type[0]
+    dialog.dataset.t2 = form.type[1] ?? form.type[0]
+
+    header.innerHTML = `<h2><span>${name}</span><span>#${id}</span></h2>`
+    header.innerHTML += html`<div class="stats">
+      <span class="form-name">${form.genus} Pokémon</span>${form.name !== species.name && form.name !== defaultForm.name
+        ? `<span class="form-name align-right">${form.name}</span></div>`
+        : ""}
+    </div>`
+
+    picture.innerHTML = `<img src="${src}" />`
+  }
+}
+
+function sortRows(col: string, type: string | undefined, node: HTMLElement): void {
+  const dir = node.dataset.dir
+  const newDir = !dir || dir === "desc" ? "asc" : "desc"
+  const rows = table.querySelectorAll(":scope tbody tr")
+
+  Array.from(rows)
+    .sort((a, b) => {
+      const [ea, eb] = dir === "asc" ? [a, b] : [b, a]
+      const [ta, tb] = [ea.querySelector(`[data-sort="${col}"]`)!, eb.querySelector(`[data-sort="${col}"]`)!]
+      const [ca, cb] = [
+        ta?.getAttribute("data-value") ?? ta.textContent,
+        tb?.getAttribute("data-value") ?? tb.textContent,
+      ]
+
+      return type === "number" ? (Number(ca) > Number(cb) ? -1 : 1) : cb.localeCompare(ca)
+    })
+    .forEach((tr) => table.querySelector("tbody")!.appendChild(tr))
+
+  table
+    .querySelectorAll("thead th")
+    .forEach((th) => (th === node ? th.setAttribute("data-dir", newDir) : th.removeAttribute("data-dir")))
+}
